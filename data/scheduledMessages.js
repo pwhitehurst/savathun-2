8dab21c7150153c85bc202e5a99842d76557b047
Paul Whitehurst <paul@whitehurstfamily.net>
2022-02-21 13:11:31 -0600

    added some surprises...

---
+++ data/scheduledMessages.js
@@ -0,0 +1,36 @@
+module.exports = {
+    messages: [
+        {
+            id: 1,
+            author: "945382142249148427",
+            channel: "553662969494372361",
+            guild: "553662969494372357",
+            date: "2022-02-22 11:00",
+            message: "Hello old friend, I've chased you for a long time..."
+        },
+        {
+            id: 2,
+            author: "945382142249148427",
+            channel: "553662969494372361",
+            guild: "553662969494372357",
+            date: "2022-02-22 12:00",
+            message: "Welcome back o' honored guest!"
+        },
+        {
+            id: 3,
+            author: "945382142249148427",
+            channel: "553662969494372361",
+            guild: "553662969494372357",
+            date: "2022-02-22 13:00",
+            message: "The game is yours to play now."
+        },
+        {
+            id: 4,
+            author: "945382142249148427",
+            channel: "553662969494372361",
+            guild: "553662969494372357",
+            date: "2022-02-22 14:00",
+            message: "Just don't say I didn't warn you."
+        }
+    ]
+}
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,21 @@
+const messages = require('../data/scheduledMessages.js').messages;
+const schedule = require('node-schedule');
+const Promise = require("bluebird");
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		Promise.map(messages, scheduledMessage => {
+			let scheduledChannel = client.channels.resolve(scheduledMessage.channel)
+			schedule.scheduleJob(
+				scheduledMessage.date,
+				() => {
+					console.log('sending messag')
+					scheduledChannel.send(scheduledMessage.message);
+				}
+			);
+		});
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
