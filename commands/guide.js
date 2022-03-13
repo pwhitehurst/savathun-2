565030353c111ffa6e8f379d04221e1a5283b4d8
Paul Whitehurst <paul@whitehurstfamily.net>
2022-03-13 14:58:40 -0500

    rearranged gude code to be data driven

--- commands/guide.js
+++ commands/guide.js
@@ -1,106 +1,51 @@
+
+const fs = require('fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
-const emojiC = '🇨';
-const emoji1 = '1️⃣';
-const emoji2 = '2️⃣';
-const emoji3 = '3️⃣';
-const emoji4 = '4️⃣';
+const guideFiles = fs.readdirSync('./data/guides').filter(file => file.endsWith('.js'));
 
-const vowEmbed = new MessageEmbed()
-	.setTitle('Vow of the Disciple Guide')
-	.setDescription('React with the guide you would like:')
-	.addFields(
-		{ name: 'Callouts', value: 'React with ' + emojiC },
-		{ name: 'Acquisition', value: 'React with ' + emoji1, inline: true },
-		{ name: 'Caretaker', value: 'React with ' + emoji2, inline: true },
-		{ name: 'Exhibition', value: 'React with ' + emoji3, inline: true },
-		{ name: 'Rhulk', value: 'React with ' + emoji4, inline: true }
-	)
-
-const vowCalloutEmbed = new MessageEmbed()
-	.setTitle('Vow of the Disciple - Caydes Shadow Callouts')
-	.setImage('https://cdn.discordapp.com/attachments/950979003492212786/950979078943539250/Caydes_Shadow_Callouts.png')
-
-const vow1Embed = new MessageEmbed()
-	.setTitle("Vow of the Disciple - Acquisition")
-	.setImage('https://i.postimg.cc/43Ygk2Z6/Vot-D-Encounter-1-V6.png');
-
-const vow2Embed1 = {
-	title: 'Vow of the Disciple - Caretaker Floor 1',
-	image: {
-		url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558023615230082/MH2gzYr.png',
-	}
+guides = {};
+base_fields = [];
+base_emoji = [];
+for (const file of guideFiles) {
+	const guide = require(`../data/guides/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	guides[guide.activity.activity_emoji] = guide.activity;
+	base_fields.push(guide.activity.main_field);
+	base_emoji.push(guide.activity.activity_emoji);
 }
 
-const vow2Embed2 = {
-	title: 'Vow of the Disciple - Caretaker Floor 2',
-	image: {
-		url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558069836480532/vSWekf9.png',
-	}
-}
 
-const vow2Embed3 = {
-	title: 'Vow of the Disciple - Caretaker Floor 3',
-	image: {
-		url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558119308296222/0S3GhDa.png',
-	}
-}
-
-const vow3Embed = new MessageEmbed()
-	.setTitle("Vow of the Disciple - Exhibition")
-	.setImage('https://i.postimg.cc/NFf6w3vV/Vot-D-Encounter-3-V6.jpg')
-
-const vow4Embed = new MessageEmbed()
-	.setTitle("Vow of the Disciple - Rhulk")
-	.setImage('https://i.postimg.cc/sXb7VRx6/Vot-D-Encounter-4-v2.png')
-
-
-async function handleVow(message, originalUser) {
-	const reply = await message.reply({ embeds: [vowEmbed], fetchReply: true });
+async function handleGuide(message, originalUser, guide) {
+	const reply = await message.reply({ embeds: guide.main_embed, fetchReply: true, failIfNotExists: false });
 	const filter = (reaction, user) => {
-		return [emojiC, emoji1, emoji2, emoji3, emoji4].includes(reaction.emoji.name) && user.id === originalUser.id;
+		return guide.emoji_used.includes(reaction.emoji.name) && user.id === originalUser.id;
 	};
 	message.delete()
-	reply.react(emojiC)
-		.then(() => reply.react(emoji1))
-		.then(() => reply.react(emoji2))
-		.then(() => reply.react(emoji3))
-		.then(() => reply.react(emoji4));
+	for (const e of guide.emoji_used) {
+		reply.react(e)
+			.catch(err => {
+				console.log('failed to react to message', err);
+			});
+	}
 
 	reply.awaitReactions({ filter, max: 1, time: 10000, errors: ['time'] })
 		.then(collected => {
 			encounter = collected.first();
-			var embedArray;
-			if (encounter.emoji.name === emojiC) {
-				embedArray = [vowCalloutEmbed];
-			} else if (encounter.emoji.name === emoji1) {
-				embedArray = [vow1Embed];
-			} else if (encounter.emoji.name === emoji2) {
-				embedArray = [vow2Embed1, vow2Embed2, vow2Embed3];
-			} else if (encounter.emoji.name === emoji3) {
-				embedArray = [vow3Embed];
-			} else if (encounter.emoji.name === 4) {
-				embedArray = [vow4Embed];
-			}
-			reply.reply({ embeds: embedArray, failIfNotExists: false })
+			reply.reply({ embeds: guide.encounters[encounter.emoji.name], failIfNotExists: false })
 			reply.delete();
 		})
 		.catch(collected => {
-			reply.reply('Please react with an appropriate emoji');
+			reply.reply({ content: 'Please react with an appropriate emoji', failIfNotExists: false });
 		});
 }
 
-const emojiV = '🇻';
-const emojiD = '🇩';
-
 const guideEmbed = new MessageEmbed()
-.setTitle('Destiny Activity Guides')
-.setDescription('React with the guide you would like:')
-.addFields(
-	{ name: 'Vow of the Disciple', value: 'React with ' + emojiV, inline: true},
-	{ name: 'Deep Stone Crypt', value: 'React with ' + emojiD, inline: true },
-);
+	.setTitle('Destiny Activity Guides')
+	.setDescription('React with the guide you would like:')
+	.addFields(base_fields);
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -108,28 +53,29 @@ module.exports = {
 		.setDescription('Get visual guides for raids')
 	,
 	async execute(interaction) {
-		message = await interaction.reply({ embeds: [guideEmbed], fetchReply: true });
+		message = await interaction.reply({ embeds: [guideEmbed], fetchReply: true, failIfNotExists: false });
 
-		message.react(emojiV).then(() => message.react(emojiD));
+		for (const e of base_emoji) {
+			message.react(e)
+				.catch(err => {
+					console.log('failed to react to message', err);
+				});
+		}
 
 		const filter = (reaction, user) => {
-			return [emojiV, emojiD].includes(reaction.emoji.name) && user.id === interaction.user.id;
+			return base_emoji.includes(reaction.emoji.name) && user.id === interaction.user.id;
 		};
 
 		message.awaitReactions({ filter, max: 1, time: 10000, errors: ['time'] })
 			.then(collected => {
 				const reaction = collected.first();
-				if (reaction.emoji.name === emojiV) {
-					return handleVow(message, interaction.user);
-				}
-				else if (reaction.emoji.name === emojiD) {
-					return message.reply('Deep Stone Crypt coming soon!');
-				} else {
-					return message.reply('unknown emoji')
+				const guide = guides[reaction.emoji.name];
+				if (guide != undefined) {
+					handleGuide(message, interaction.user, guide);
 				}
 			})
 			.catch(collected => {
-				message.reply('You reacted with neither 🇻, nor 🇩.');
+				message.reply({ content: 'You reacted with neither 🇻, nor 🇩.', failIfNotExists: false });
 				message.delete();
 			});
 	}
---
+++ data/guides/vow_guide.js
@@ -0,0 +1,84 @@
+const emojiC = '🇨';
+const emoji1 = '1️⃣';
+const emoji2 = '2️⃣';
+const emoji3 = '3️⃣';
+const emoji4 = '4️⃣';
+
+module.exports = {
+    activity: {
+        activity_emoji: '🅿️',
+        main_field: { name: 'Vow of the Disciple', value: 'React with 🅿️', inline: true },
+        main_embed: [
+            {
+                title: 'Vow of the Disciple Guide',
+                description: 'React with the guide you would like:',
+                fields: [
+                    { name: 'Callouts', value: 'React with ' + emojiC },
+                    { name: 'Acquisition', value: 'React with ' + emoji1, inline: true },
+                    { name: 'Caretaker', value: 'React with ' + emoji2, inline: true },
+                    { name: 'Exhibition', value: 'React with ' + emoji3, inline: true },
+                    { name: 'Rhulk', value: 'React with ' + emoji4, inline: true }
+                ],
+            }
+        ],
+        emoji_used: [
+            emojiC,
+            emoji1,
+            emoji2,
+            emoji3,
+            emoji4
+        ],
+        encounters: {
+            '🇨': [
+                {
+                    title: 'Vow of the Disciple - Caydes Shadow Callouts',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/950979003492212786/950979078943539250/Caydes_Shadow_Callouts.png'
+                    }
+                }
+            ],
+            '1️⃣': [{
+                title: 'Vow of the Disciple - Acquisition',
+                image: {
+                    url: 'https://i.postimg.cc/43Ygk2Z6/Vot-D-Encounter-1-V6.png'
+                }
+            }],
+            '2️⃣': [
+                {
+                    title: 'Vow of the Disciple - Caretaker Floor 1',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558023615230082/MH2gzYr.png',
+                    }
+                },
+                {
+                    title: 'Vow of the Disciple - Caretaker Floor 2',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558069836480532/vSWekf9.png',
+                    }
+                },
+                {
+                    title: 'Vow of the Disciple - Caretaker Floor 3',
+                    image: {
+                        url: 'https://cdn.discordapp.com/attachments/950979003492212786/951558119308296222/0S3GhDa.png',
+                    }
+                }
+            ],
+            '3️⃣': [
+                {
+                    title: 'Vow of the Disciple - Exhibition',
+                    image: {
+                        url: 'https://i.postimg.cc/NFf6w3vV/Vot-D-Encounter-3-V6.jpg'
+                    }
+                }
+            ],
+            '4️⃣': [
+                {
+                    title: 'Vow of the Disciple - Rhulk',
+                    image: {
+                        url: 'https://i.postimg.cc/sXb7VRx6/Vot-D-Encounter-4-v2.png'
+                    }
+                }
+            ]
+        }
+    }
+}
